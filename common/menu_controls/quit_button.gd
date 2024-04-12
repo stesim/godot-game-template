58472b4cbca603d8d25c902e053db88dd67a2798
stesim <simeonov.stefan@gmail.com>
2024-04-12 21:56:04 +0200

    added option to quit button for removing them from web builds

--- common/menu_controls/quit_button.gd
+++ common/menu_controls/quit_button.gd
@@ -1,7 +1,15 @@
 extends "game_menu_button.gd"
 
 
+@export var remove_in_web_export := true
+
+
 func _ready() -> void:
 	super()
+
+	if remove_in_web_export and OS.has_feature(&"web"):
+		hide()
+		queue_free()
+		return
 
 	pressed.connect(get_tree().quit)
